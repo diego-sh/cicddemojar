d8ee5b65576d790d3274b6245f3bb902bbb88855
ALTIORACORP\dalpala <Diego Alpala>
2025-04-11 14:34:22 -0500

    feat: nueva Api

--- src/main/java/com/example/demojar/Demo1Controlador.java
+++ src/main/java/com/example/demojar/Demo1Controlador.java
@@ -3,6 +3,7 @@ package com.example.demojar;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,32 @@ public class Demo1Controlador {
     @GetMapping("/holamundo")
     public ResponseEntity holamundo() {
         return ResponseEntity.ok(Boolean.TRUE);
+    }
+
+    @GetMapping("/numeroParImpar")
+    public ResponseEntity holamundo(@RequestParam int numero) {
+
+        return ResponseEntity.ok(this.metodoComplejo(numero));
+    }
+
+    private String metodoComplejo( int numero) {
+       
+            if (numero % 2 == 0) {
+                return "Numero Par";
+            } else {
+                return "Numero Impar";
+            }
+        
+    }
+    
+    private String metodoComplejo2( int numero) {
+       
+        if (numero % 2 == 0) {
+            return "Numero Par";
+        } else {
+            return "Numero Impar";
+        }
+    
     }
 
 }
